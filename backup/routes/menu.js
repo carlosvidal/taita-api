062b9d025273d56956383e311e50cebd0ef22d33
Carlos Vidal <carlos@mitienda.pe>
2025-04-11 08:15:59 -0500

    borré backups y obsolete

--- backup/routes/menu.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const menuController = require("../controllers/menuController");
-
-const router = express.Router();
-
-router.post("/menu", menuController.createMenuItem);
-
-module.exports = router;
